389588004fd37d302e2f7a8cb05c435c84eaf49e
Fenion <38190817+Fenion@users.noreply.github.com>
2020-02-12 21:10:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ fs.readdir('./cmds/', (error, files) => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity('KurtzPay');
+    client.user.setActivity('p!help');
 });
 
 client.on('message', (msg) => {
